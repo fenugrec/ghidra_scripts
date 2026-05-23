791f2bd05d8425eb14fcf9d73479e94873e073ea
fenugrec <fenugrec@users.sourceforge.net>
2026-05-23 17:29:58 -0400

    mc68332: also define regs

--- mc68332_defs.py
+++ mc68332_defs.py
@@ -12,6 +12,47 @@
 # @author: fenugrec
 # @category: mc68k
 
+import collections
+#from dataclasses import dataclass crap, doesnt work with jython ?
+import csv
+import os	#for os.path.join
+import glob
+
+module_baseaddr = 0xfffff000    # TODO : baseaddr should depend on 'MM' bit in SIMCR
+
+# @dataclass
+# class mmio_area():
+#     name: str
+#     offs: int
+#     len: int
+# 
+class mmio_area():
+    def __init__(self, name, base, len):
+        self.name = name
+        self.base = base
+        self.len = len
+
+mc68332_mmio = [
+        mmio_area('SIM', 0xa00, 0x80),
+        mmio_area('TPURAM_CTL', 0xb00, 0x40),
+        mmio_area('QSM', 0xc00, 0x200),
+        mmio_area('TPU', 0xe00, 0x200),
+        ]
+
+# TODO: implement, or convert to dataclass
+devtype = collections.namedtuple('CPU', ['cpu', 'mmio_map', 'regs_csv'])
+devlist = [
+        devtype('mc68332', mc68332_mmio, 'mc68332regs.csv'),
+        ]
+
+def get_builtin_defs():
+#for some reason the "current directory" for open() is not the script's location.
+    script_location = os.path.dirname(sourceFile.getAbsolutePath())
+    flist = [os.path.basename(a) for a in glob.glob(os.path.join(script_location, '*.csv'))]
+    fname = askChoice("register defs", "Select register definition CSV file", flist, flist[0])
+    return os.path.join(script_location, fname)
+
+
 # create one MMIO mem block : uninit, RW and volatile
 def mmioblock_helper(base, offs, len, name):
     block = createMemoryBlock(name, toAddr(base + offs), None, len, 0)
@@ -20,31 +61,32 @@ def mmioblock_helper(base, offs, len, name):
     return
 
 
-# define MMIO arease and peripheral regs
+# define MMIO areas
 # TODO : baseaddr should depend on 'MM' bit in SIMCR
-def create_ioregs(module_baseaddr = 0xfffff000):
+def create_iomap(module_baseaddr = 0xfffff000):
     mmioblock_helper(module_baseaddr, 0xa00, 0x80, 'SIM')
     mmioblock_helper(module_baseaddr, 0xb00, 0x40, 'TPURAM_CTL')
     mmioblock_helper(module_baseaddr, 0xc00, 0x200, 'QSM')
     mmioblock_helper(module_baseaddr, 0xe00, 0x200, 'TPU')
     return
 
-def unused():
-    #for some reason the "current directory" for open() is not the script's location.
-    script_location = os.path.dirname(sourceFile.getAbsolutePath())
-    csv_filename = os.path.join(script_location, devtype_base.regs_csv)
-
+#open definitions file and apply at base address
+def define_regs(base, csv_filename):
     with open(csv_filename, 'rb') as f:
         reader = csv.DictReader(f)
         for row in reader:
-            reg_name = row['reg_name']
-            #print reg_name, row['reg_addr']
-            reg_addr = int(row['reg_addr'], base=16)
-            # create as Primary label
-            createLabel(toAddr(reg_addr), reg_name, 1)
+            offs = int(row['base_offset'], base=16)
+            regname = row['regname']
+
+			# create as Primary label
+            addr = toAddr(base + offs)
+            createLabel(addr, regname, 1)
+            setEOLComment(addr, row['comment'])
 
 def main():
-    create_ioregs()
+    csvfile = get_builtin_defs()
+    create_iomap()
+    define_regs(module_baseaddr, csvfile)
     return
 
 
